92f043933ae9d3895b2e1b0f62a77f532c7245a1
Yaser Al-Ananbeh <yaser.ananbeh74@gmail.com>
2021-06-26 17:22:22 +0300

    designing the profile page

--- src/components/CartSection.js
+++ src/components/CartSection.js
@@ -5,57 +5,88 @@ import Col from "react-bootstrap/Col";
 import cartIcon from "../assets/images/cartIcon.png";
 import deletePro from "../assets/images/deletePro.png";
 import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 export class cartSection extends Component {
   render() {
     return (
       <div>
-        <Container style={{ textAlign: "center" }}>
-          <Row>
-            <Col>
-              <h2>My Cart</h2>
-            </Col>
-            <Col>
-              <img src={cartIcon} alt="carIcon" />
-            </Col>
-          </Row>
-
-          <Row>
-         
-            <Col>
-            <Row style={{ border:'2px solid black',padding:'5px' ,margin:'5px 0'}}>
-                <Col>Product</Col>
-                <Col>Qunatity</Col>
-                <Col>delete</Col>
-              </Row>
-              <Row style={{border:'2px solid black' , padding:'5px' ,margin:'5px 0'}}>
-                <Col>PR Name</Col>
-                <Col>Qunatity</Col>
-                <Col><img src={deletePro} alt="delete" /></Col>
-              </Row>
-
-              <Row style={{border:'2px solid black' , padding:'5px' ,margin:'5px 0'}}>
-                <Col>PR Name</Col>
-                <Col>Qunatity</Col>
-                <Col><img src={deletePro} alt="delete" /></Col>
-              </Row>
-
-              <Row style={{border:'2px solid black' , padding:'5px' ,margin:'5px 0'}}>
-                <Col>PR Name</Col>
-                <Col>Qunatity</Col>
-                <Col><img src={deletePro} alt="delete" /></Col>
-              </Row>
-
-              <Row>
-                <Col></Col>
-                <Col></Col>
+        <Card className="text-center" >
+          <Card.Header>Checkout Cart</Card.Header>
+          <Card.Body className="cartContainerBody">
+            <Container className='cartMainContainer'>
+              <Row className='cartContainerHeader'>
                 <Col>
-                  <Button variant="outline-warning">Checkout</Button>
+                  <h2>My Cart</h2>
+                </Col>
+                <Col>
+                  <img src={cartIcon} alt="carIcon" />
                 </Col>
               </Row>
-            </Col>
-          </Row>
-        </Container>
+              <Row>
+                <Col>
+                  <Row style={{ border: '2px solid black', padding: '5px', margin: '5px 0' }}>
+                    <Col>Product</Col>
+                    <Col>Qunatity</Col>
+                    <Col>delete</Col>
+                  </Row>
+                  <Row style={{ border: '2px solid black', padding: '5px', margin: '5px 0' }}>
+                    <Col>PR Name</Col>
+                    <Col>Qunatity</Col>
+                    <Col><img src={deletePro} alt="delete" /></Col>
+                  </Row>
+                  <Row style={{ border: '2px solid black', padding: '5px', margin: '5px 0' }}>
+                    <Col>PR Name</Col>
+                    <Col>Qunatity</Col>
+                    <Col><img src={deletePro} alt="delete" /></Col>
+                  </Row>
+                  <Row style={{ border: '2px solid black', padding: '5px', margin: '5px 0' }}>
+                    <Col>PR Name</Col>
+                    <Col>Qunatity</Col>
+                    <Col><img src={deletePro} alt="delete" /></Col>
+                  </Row>
+                  <Row style={{ border: '2px solid black', padding: '5px', margin: '5px 0' }}>
+                    <Col>PR Name</Col>
+                    <Col>Qunatity</Col>
+                    <Col><img src={deletePro} alt="delete" /></Col>
+                  </Row>
+                  <Row style={{ border: '2px solid black', padding: '5px', margin: '5px 0' }}>
+                    <Col>PR Name</Col>
+                    <Col>Qunatity</Col>
+                    <Col><img src={deletePro} alt="delete" /></Col>
+                  </Row>
+                  <Row style={{ border: '2px solid black', padding: '5px', margin: '5px 0' }}>
+                    <Col>PR Name</Col>
+                    <Col>Qunatity</Col>
+                    <Col><img src={deletePro} alt="delete" /></Col>
+                  </Row>
+                  <Row style={{ border: '2px solid black', padding: '5px', margin: '5px 0' }}>
+                    <Col>PR Name</Col>
+                    <Col>Qunatity</Col>
+                    <Col><img src={deletePro} alt="delete" /></Col>
+                  </Row>
+                  <Row style={{ border: '2px solid black', padding: '5px', margin: '5px 0' }}>
+                    <Col>PR Name</Col>
+                    <Col>Qunatity</Col>
+                    <Col><img src={deletePro} alt="delete" /></Col>
+                  </Row>
+                  <Row style={{ border: '2px solid black', padding: '5px', margin: '5px 0' }}>
+                    <Col>PR Name</Col>
+                    <Col>Qunatity</Col>
+                    <Col><img src={deletePro} alt="delete" /></Col>
+                  </Row>
+                  <Row>
+                    <Col>
+                      <Button variant="outline-warning">Checkout</Button>
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>
+            </Container>
+          </Card.Body>
+
+        </Card>
+
       </div>
     );
   }
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,28 +3,39 @@ import CartSection from "./CartSection";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+// import Col from "react-bootstrap/Col";
 
 
 export class profile extends Component {
   render() {
     return (
       <div>
-        <Container style={{ textAlign: "center" }}>
-          <Row>
-            <Col>
-              <img src="https://via.placeholder.com/200x200/000000" alt="" />
-            </Col>
-            <Col>
-              <div>
-                <h2>User Name</h2>
+        <Card className="text-center" >
+          <Card.Header>Profile Information</Card.Header>
+          <Card.Body className="profileInfoContainer">
+            <Container fluid style={{ textAlign: "center" }} >
+              <Row>
+                <Col>
+                  <img src="https://via.placeholder.com/1500x1500/000000" alt="" className='profileInfoImg'/>
+                </Col>
+                <Col>
+                  <Row className="profileInfoText">
+                    <div>
+                      <h2>Yaser Al-Ananbeh</h2>
+                      <h4>yaser.ananbeh74@gmail.com</h4>
+                    </div>
+                  </Row>
+                </Col>
+              </Row>
+            </Container>
 
-                <h4>Email</h4>
-              </div>
-            </Col>
-          </Row>
-        </Container>
+          </Card.Body>
+
+        </Card>
+
         <CartSection />
-     
+
       </div>
     );
   }
