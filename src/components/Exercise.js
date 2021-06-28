6457548675b52c9520a8870d6a4f84796e55688f
Yaser Al-Ananbeh <yaser.ananbeh74@gmail.com>
2021-06-28 10:06:32 +0300

    add the images to the product page

--- src/components/Exercise.js
+++ src/components/Exercise.js
@@ -2,17 +2,14 @@ import React, { Component } from "react";
 import Card from "react-bootstrap/Card";
 
 class Exercise extends Component {
-  // let regex = /0/g;
-  // let newString = string.replace(regex, "zero");
-  // return newString;
 
   render() {
     return (
       <Card style={{ width: "100%" }}>
-        <Card.Img variant="top" src="holder.js/100px180" alt={this.props.name} />
+        <Card.Img variant="top" src="https://image.freepik.com/free-vector/coming-soon-shop-announcement-vector_53876-57828.jpg" alt={this.props.name} />
         <Card.Body>
           <Card.Title>{this.props.name}</Card.Title>
-          <Card.Text>{this.props.desc}</Card.Text>
+          <Card.Text style={{maxHeight:"20vh",overflowY:"scroll"}}>{this.props.desc}</Card.Text>
         </Card.Body>
       </Card>
     );
